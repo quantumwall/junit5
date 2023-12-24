d2e093f515db79283451d277300902c47c14e91a
quantumwall <liberiamor@gmail.com>
2023-12-24 11:12:11 +0300

    init commit

---
+++ src/test/java/org/quantum/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package org.quantum.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    void failTest() {
+        Assertions.fail();
+    }
+}
